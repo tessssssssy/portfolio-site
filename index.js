2a580ae6decc3629fe0a39d29266f47aea4538b3
tessssssssy <tessfabry@gmail.com>
2020-04-01 12:30:51 +1100

    mobile nav click listener

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-$(document).ready(function(){
-	$('.slider').on('click', function(){
-		
-		if ( $('.active').next('.slide').length ){
-			$('.active').removeClass('active')
-				.next('.slide')
-				.addClass('active');
+var hamburger = document.querySelector("#hamburger");
+var links = document.querySelectorAll(".nav-link");
+
+console.log("javascript file!");
+console.log(hamburger);
+console.log(links);
+
+// hamburger.addEventListener("click", function() 
+// { console.log("Hello World!"); });
+
+hamburger.addEventListener("click", function() {
+	console.log("CLICK WORKING!");
+	for (var i = 0; i < links.length; i++) {
+		if (links[i].style.display === 'none') {
+			links[i].style.display = 'block'
 		} else {
-			$('.active').removeClass('active');
-			$('.slide').first().addClass('active');
+			links[i].style.display = 'none'
 		}
-		
-	});
-});
+	}	
+})
 
